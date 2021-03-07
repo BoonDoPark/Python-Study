078ef83e9a9e49dcbdb6ef3d3a343652daa70358
Ryu0n <32003817+Ryu0n@users.noreply.github.com>
2021-03-07 21:50:32 +0900

    Try~Except.

---
+++ HelloWorld/Course/2021-03-07.py
@@ -0,0 +1,26 @@
+# try:
+#     4 / 0
+# except ZeroDivisionError as e:
+#     print(e)
+#
+# print('aaaa')
+
+# try:
+#     f = open('a.txt', 'r')
+
+f = None
+try:
+    f = open('a.txt', 'r')
+    # f = open('b.txt', 'r')
+except FileNotFoundError as e:
+    print(e)
+finally:
+    print('finally', f)
+    if f:
+        while True:
+            line = f.readline()
+            if not line: break
+            print(line)
+
+        print('im closed')
+        f.close()
