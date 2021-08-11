f2844f0c4521a6d251d6c28c8a071a00b3e01c0a
BoonDoPark <76871728+BoonDoPark@users.noreply.github.com>
2021-08-11 15:39:34 +0900

    Update Calculator.py

--- Calculator/src/Calculator.py
+++ Calculator/src/Calculator.py
@@ -1,81 +1,18 @@
 class Calculator:
-    def __init__(self, a, b):
-        # self.select = input().split(' ')
-        self.a = a
-        self.b = b
 
-    def add(self):
-        # a = 0
-        # for i in self.select:
-        #    a += int(i)
-        return self.a + self.b
+    @staticmethod
+    def add(first_dig, second_dig):
+        return first_dig + second_dig
 
-    def sub(self):
-        # s = (int(self.select[0]) + int(self.select[0]))
-        # for i in self.select:
-        #    s -= int(i)
-        return self.a - self.b
+    @staticmethod
+    def sub(first_dig, second_dig):
+        return first_dig - second_dig
 
-    def mul(self):
-        # m = 1
-        # for i in self.select:
-        #    m *= int(i)
-        return self.a * self.b
+    @staticmethod
+    def mul(first_dig, second_dig):
+        return first_dig * second_dig
 
-    def div(self):
-        # d = int(self.select[0])*int(self.select[0])
-        # for i in self.select:
-        #    d /= int(i)
-        return self.a / self.b
+    @staticmethod
+    def div(first_dig, second_dig):
+        return first_dig / second_dig
 
-    def input(self):
-        self.c = int(input())
-        return self.c
-
-    def add_process(self):
-        add = self.add()
-        input = self.input()
-        self.d = add + input
-        print(self.d)
-        while True:
-            input = self.input()
-            if input == 0:
-                break
-            self.d += input
-            return self.d
-
-    def sub_process(self):
-        sub = self.sub()
-        input = self.input()
-        self.d = sub - input
-        print(self.d)
-        while True:
-            input = self.input()
-            if input == 0:
-                break
-            self.d -= input
-            return self.d
-
-    def mul_process(self):
-        mul = self.mul()
-        input = self.input()
-        self.d = mul * input
-        print(self.d)
-        while True:
-            input = self.input()
-            if input == 0:
-                break
-            self.d *= input
-            return self.d
-
-    def div_process(self):
-        div = self.div()
-        input = self.input()
-        self.d = div / input
-        print(self.d)
-        while True:
-            input = self.input()
-            if input == 0:
-                break
-            self.d /= input
-            return self.d
